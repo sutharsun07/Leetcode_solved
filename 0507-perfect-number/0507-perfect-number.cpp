8cd227dc0a25638c4a1d327e70237ad5bea6e7ab
SUTHARSUN S 24CSR315 <sutharsun781845@gmail.com>
2026-03-07 15:56:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=1;
+        for(int i=2;i*i <= num;i++){
+            if(num%i == 0){
+                if(i == num/i){
+                    sum+=i;
+                }
+                else{
+                  sum+=(i + num/i);
+                }
+            }
+        }
+        return sum == num;
+    }
+};
