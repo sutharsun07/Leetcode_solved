163a8aacca5a70408685ab302437c900298c97b8
SUTHARSUN S 24CSR315 <sutharsun781845@gmail.com>
2026-06-08 12:01:10 +0530

    Time: 48 ms (10.31%), Space: 25.3 MB (22.03%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -2,16 +2,9 @@ class Solution {
 public:
     int maxDistinct(string s) {
         set<char> ans;
-        int count=0;
         for(int i=0;i<s.size();i++){
-            if(ans.contains(s[i])){
-                
-            }
-            else{
-                count++;
-                ans.insert(s[i]);
-            }
+            ans.insert(s[i]);
         }
-        return count;
+        return ans.size();
     }
 };
