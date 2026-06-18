9376dda549bd17f587ba035b361ed52497982bb4
SUTHARSUN S 24CSR315 <sutharsun781845@gmail.com>
2026-06-18 12:00:49 +0530

    Time: 0 ms (100%), Space: 8.1 MB (46.25%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double h=(hour%12)*30 + minutes*0.5;
+        double m=minutes*6;
+        double diff=abs(h-m);
+        return min(diff,360-diff);
+    }
+};
