7e6952c3e2e76791c95f34eba1ed8d1cf4a319c4
SUTHARSUN S 24CSR315 <sutharsun781845@gmail.com>
2026-06-19 09:39:24 +0530

    Time: 0 ms (100%), Space: 10.7 MB (73.87%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        vector<int> ans;
-        ans.push_back(0);
         int sum=0;
         int mx=0;
         for(int i=0;i<gain.size();i++){
